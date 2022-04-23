760b39a7854ac2bcd21d00245160bedfe5cc2fc7
JaeHyun Ahn <97944429+JaeHyunGround@users.noreply.github.com>
2022-04-24 00:14:12 +0900

    프론트 스터디 - 생활코딩 WEB2 JavaScript 1 ~ 13

---
+++ 프론트 스터디/생활코딩1주차/js/프론트 스터디/생활코딩1주차/js/7_tagcontrol.js
@@ -0,0 +1,15 @@
+document.write("<div>7장 제어할 태그 선택</div>");
+
+// document.querySelcetor('선택자')로 제어 가능!
+// .속성."속성의 속성 = 값"
+
+
+// 실습 : night 버튼 생성 후 클릭 이벤트로 배경 색바꾸기
+
+// querySelector 와 getElementById 의 차이점?
+// =>  getElementById 가 querySelector보다 조금 더 빠르다!
+
+// document.querySelector('선택자')
+// => 선택자에는 태그, class, id 가 올 수 있다.
+// => class 는 .~~
+// => id는 #~~
